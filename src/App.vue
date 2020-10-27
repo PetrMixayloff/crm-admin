af188827ef4bdab17ddcb2927c8a45b60dfaab5d
Petr <p>
2020-10-27 15:50:00 +0300

    fix localization

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { loadMessages, locale } from 'devextreme/localization'
+import { messagesRu } from './const'
 
 @Component({
   name: 'App'
 })
 export default class extends Vue {
   created() {
+    loadMessages(messagesRu)
+    locale('ru')
   }
 }
 </script>
