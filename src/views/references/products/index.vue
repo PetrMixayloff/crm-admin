a092be643a37fd9a190782decc906a8e2989f8e0
Petr <p>
2020-10-28 17:37:10 +0300

    start product view

--- src/views/references/products/index.vue
+++ src/views/references/products/index.vue
@@ -1,14 +1,38 @@
 <template>
   <div class="app-container">
-    <p>Страница в разработке</p>
+    <div class="flex-l-t">
+      <el-card>
+        <DxTreeView
+          id="simple-treeview"
+          :items="products"
+          :width="300"
+          :search-enabled="true"
+          search-mode="contains"
+          @item-click="selectItem"
+        />
+      </el-card>
+      <el-card style="width: 100%;">
+      </el-card>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import DxTreeView from 'devextreme-vue/tree-view'
 
 @Component({
-  name: 'Products'
+  name: 'Products',
+  components: {
+    DxTreeView
+  }
 })
-export default class extends Vue {}
+export default class extends Vue {
+  public products = []
+  public currentItem = {}
+
+  selectItem(e: any) {
+    this.currentItem = e.itemData
+  }
+}
 </script>
